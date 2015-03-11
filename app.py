86e9846102dfe215045a68b1860002255eaa9409
DavidCA916 <davidcackette@gmail.com>
2015-03-11 15:24:30 -0700

    api keys

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ app.secret_key = os.environ.get('SECRET_KEY')
 
 # configure Instagram API
 instaConfig = {
-	'client_id':os.environ.get('CLIENT_ID'),
-	'client_secret':os.environ.get('CLIENT_SECRET'),
-	'redirect_uri' : os.environ.get('REDIRECT_URI')
+	'client_id':'a3010e12a8fa4f428541358d83a0716a',
+	'client_secret':'c245cc783d4743caa9032a3c23afac6b',
+	'redirect_uri' : 'https://shielded-brushlands-9106.herokuapp.com//instagram_callback'
 }
 api = InstagramAPI(**instaConfig)
 
