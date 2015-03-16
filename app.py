68870fdd02571cac3cb8afe9f61cb36c38501ace
DavidCA916 <davidcackette@gmail.com>
2015-03-16 14:39:27 -0700

    added users feed

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def popular_photos():
 	# if instagram info is in session variables, then display popular photos
 	if 'instagram_access_token' in session:
 		userAPI = InstagramAPI(access_token=session['instagram_access_token'])
-		media_search = api.media_popular()
+		media_search = api.media_popular(count=25)
 
 		templateData = {
 			'size' : request.args.get('size','thumb'),
@@ -55,6 +55,23 @@ def popular_photos():
 		return redirect('/connect')
 
 
+@app.route('/feed')
+def feed():
+
+	# if instagram info is in session variables, then display popular photos
+	if 'instagram_access_token' in session:
+		userAPI = InstagramAPI(access_token=session['instagram_access_token'])
+		user_feed = api.user_media_feed(count=25)
+
+		templateData = {
+			'size' : request.args.get('size','thumb'),
+			'media' : media_search
+		}
+
+		return render_template('display.html', **templateData)
+
+	else:
+		return redirect('/connect')
 
 
 # Redirect users to Instagram for login
