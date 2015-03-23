b687f1e698b863b5773e42920aab7ce296137cab
DavidCA916 <davidcackette@gmail.com>
2015-03-23 14:51:28 -0700

    added profile pic

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ def home():
 		user_info = userAPI.user(user_id=session['instagram_user'].get('id'))
 
 		templateData = {
-			'username' : user_info
+			'username' : user_info,
+			'profile_picture': user_info
 		}
 
 		return render_template('home.html', **templateData)
